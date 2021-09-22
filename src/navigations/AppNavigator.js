fd8f81ca37adacfaf3891c82b2418f35210aa6f3
Tudor <serban88@gmail.com>
2021-09-22 12:42:50 +0300

    add assets

--- src/navigations/AppNavigator.js
+++ src/navigations/AppNavigator.js
@@ -8,9 +8,13 @@ import HistoryScreen from '_scenes/HistoryScreen'
 import { StyleSheet } from 'react-native'
 import { Colors } from '_styles'
 import OffersIcon from '_assets/images/offers.svg'
+import OffersActiveIcon from '_assets/images/offers-active.svg'
 import HistoryIcon from '_assets/images/history.svg'
+import HistoryActiveIcon from '_assets/images/history-active.svg'
 import HomeIcon from '_assets/images/home.svg'
+import HomeActiveIcon from '_assets/images/home-active.svg'
 import StoresIcon from '_assets/images/stores.svg'
+import StoresActiveIcon from '_assets/images/stores-active.svg'
 import SettingsIcon from '_assets/images/settings.svg'
 import TabIcon from '_atoms/TabIcon'
 import { AuthStoreContext } from '_stores/index.js'
@@ -39,43 +43,46 @@ const RootStack = createStackNavigator()
 const Tabs = () => (
   <Tab.Navigator sceneContainerStyle={styles.cardStyle}
                  initialRouteName={'Home'}
-                 tabBar={props => <TabBar {...props} />}
+                 // tabBar={props => <TabBar {...props} />}
                  tabBarOptions={{
                      style: {
                          minHeight: 80,
                      },
-                     showLabel: false
+                     showLabel: true
+                 }}
+                 screenOptions={{
+                     tabBarActiveTintColor: Colors.PRIMARY,
                  }}
   >
       <Tab.Screen name={'Home'} component={HomeNavigator}
                   options={{
-                      tabBarLabel: '',
-                      tabBarIcon: ({ focused }) => <TabIcon focused={focused} label={'Home'} icon={<HomeIcon fill={focused ? Colors.PRIMARY : Colors.GRAY_DARK}/>}/>
+                      tabBarLabel: 'Home',
+                      tabBarIcon: ({ focused }) => <TabIcon focused={focused} label={'Home'} icon={focused ? <HomeActiveIcon/> : <HomeIcon/>}/>
                   }}
       />
       <Tab.Screen name="Stores" component={StoresScreen}
                   options={{
-                      tabBarLabel: '',
-                      tabBarIcon: ({ focused }) => <TabIcon focused={focused} label={'Stores'} icon={<StoresIcon fill={focused ? Colors.PRIMARY : Colors.GRAY_DARK}/>}/>
+                      tabBarLabel: 'Stores',
+                      tabBarIcon: ({ focused }) => <TabIcon focused={focused} label={'Stores'} icon={focused ? <StoresActiveIcon/> : <StoresIcon/>}/>
                   }}
       />
       <Tab.Screen name="Offers" component={OffersScreen}
                   options={{
-                      tabBarLabel: '',
-                      tabBarIcon: ({ focused }) => <TabIcon focused={focused} label={'Offers'} icon={<OffersIcon fill={focused ? Colors.PRIMARY : Colors.GRAY_DARK}/>}/>
+                      tabBarLabel: 'Offers',
+                      tabBarIcon: ({ focused }) => <TabIcon focused={focused} label={'Offers'} icon={focused ? <OffersActiveIcon/> : <OffersIcon/>}/>
                   }}
       />
 
       <Tab.Screen name="History" component={HistoryScreen}
                   options={{
-                      tabBarLabel: '',
-                      tabBarIcon: ({ focused }) => <TabIcon focused={focused} label={'History'} icon={<HistoryIcon fill={focused ? Colors.PRIMARY : Colors.GRAY_DARK}/>}/>
+                      tabBarLabel: 'History',
+                      tabBarIcon: ({ focused }) => <TabIcon focused={focused} label={'History'} icon={focused ? <HistoryActiveIcon/> : <HistoryIcon/>}/>
                   }}
       />
       <Tab.Screen name="AccountSettings" component={AccountNavigator}
                   options={{
-                      tabBarLabel: '',
-                      tabBarIcon: ({ focused }) => <TabIcon focused={focused} label={'Settings'} icon={<SettingsIcon fill={focused ? Colors.PRIMARY : Colors.GRAY_DARK}/>}/>
+                      tabBarLabel: 'Account',
+                      tabBarIcon: ({ focused }) => <TabIcon focused={focused} label={'Settings'} icon={<SettingsIcon/>}/>
                   }}
       />
   </Tab.Navigator>
