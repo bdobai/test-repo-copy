c0d8493dd188e2bc22c6ef7463ddb2d06950024f
Tudor Serban <serban88@gmail.com>
2022-01-11 11:54:30 +0000

    Update mixins.js

--- src/styles/mixins.js
+++ src/styles/mixins.js
@@ -1,6 +1,6 @@
 import { Dimensions, PixelRatio } from 'react-native'
 
-const WINDOW_WIDTH = Dimensions.get('window').width
+export const WINDOW_WIDTH = Dimensions.get('window').width
 export const SCREEN_HEIGHT = Dimensions.get('screen').height
 export const WINDOW_HEIGHT = Dimensions.get('window').height
 const guidelineBaseWidth = 414
